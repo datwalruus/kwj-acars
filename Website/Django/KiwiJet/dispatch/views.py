56301fe99d6cec5c09706eb752cbeea399ab4eb1
datwalruus <106207718+datwalruus@users.noreply.github.com>
2023-10-19 19:16:14 +1300

    DJANGO TIME, #helloworld

---
+++ Website/Django/KiwiJet/dispatch/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def dispatch(request):
+    template = loader.get_template("dispatch.html")
+    return HttpResponse("Hallo Welt ")
